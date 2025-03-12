a4e136f341cfb173d82c9cc971895790de88b6f2
Paul Abbott <phatpaul@gmail.com>
2025-03-12 11:31:15 -0400

    Cgi response common multi cleanup (#105)
    * added error return to cgiGetArg* helper functions
    
    * cgiJsonResponse don't pretty-print output
    
    * Refactored cgiResponseCommonMulti() out of cgiJsonResponseCommonMulti.
    Added cgiResponseCommonMultiCleanup to avoid memory leak.
    
    ---------
    
    Co-authored-by: Paul Abbott <pabbott@lumiteclighting.com>

--- include/libesphttpd/cgi_common.h
+++ include/libesphttpd/cgi_common.h
@@ -6,37 +6,26 @@
 #include "libesphttpd/httpd.h"
 #include "cJSON.h"
 
-// Parses *allArgs (i.e. connData->getArgs or connData->post.buff) for a signed integer by name of *argName and returns int value at *pvalue.  
-bool cgiGetArgDecS32(const char *allArgs, const char *argName, int *pvalue, char *buff, int buffLen);
-// Parses *allArgs (i.e. connData->getArgs or connData->post.buff) for a unsigned int (i.e. ?uintval=123)
-bool cgiGetArgDecU32(const char *allArgs, const char *argName, uint32_t *pvalue, char *buff, int buffLen);
-// Parses *allArgs (i.e. connData->getArgs or connData->post.buff) for a uint32_t from a hexadecimal string (i.e. ?hexval=0123ABCD )
-bool cgiGetArgHexU32(const char *allArgs, const char *argName, uint32_t *pvalue, char *buff, int buffLen);
-// Parses *allArgs (i.e. connData->getArgs or connData->post.buff) for a string value.  (just a wrapper for httpdFindArg())
-bool cgiGetArgString(const char *allArgs, const char *argName, char *buff, int buffLen);
+// this should be more efficient than httpdSend(,,-1) using strlen() at runtime on constant strings
+#define HTTP_SEND_CONST(connData, constString) httpdSend(connData, constString, sizeof(constString)-1) //-1 to skip sending the null-terminator
 
+enum {
+    CGI_ARG_ERROR = -1,
+    CGI_ARG_NOT_FOUND = 0,
+    CGI_ARG_FOUND = 1
+};
+int cgiGetArgDecS32(const char *allArgs, const char *argName, int *pvalue, char *buff, int buffLen);
+int cgiGetArgDecU32(const char *allArgs, const char *argName, uint32_t *pvalue, char *buff, int buffLen);
+int cgiGetArgHexU32(const char *allArgs, const char *argName, uint32_t *pvalue, char *buff, int buffLen);
+int cgiGetArgString(const char *allArgs, const char *argName, char *buff, int buffLen);
 
 void cgiJsonResponseHeaders(HttpdConnData *connData);
 void cgiJavascriptResponseHeaders(HttpdConnData *connData);
 
-/**
- * Example usage of cgiJsonResponseCommonMulti for multipart json response (i.e. larger than 1kB)
- * 
-CgiStatus cgiFn(HttpdConnData *connData)
-{
-    cJSON *jsroot = NULL;
-    if (connData->cgiData == NULL)
-    {
-        //First call to this cgi.
-        jsroot = cJSON_CreateObject();
-        ...
-        cgiJsonResponseHeaders(connData);
-    }
-    return cgiJsonResponseCommonMulti(connData, &connData->cgiData, jsroot); // Send the json response!
-}
- */
+CgiStatus cgiResponseCommonMulti(HttpdConnData *connData, void **statepp, const char *toSendAndFree);
 CgiStatus cgiJsonResponseCommonMulti(HttpdConnData *connData, void **statepp, cJSON *jsroot);
 CgiStatus cgiJsonResponseCommonSingle(HttpdConnData *connData, cJSON *jsroot);
+CgiStatus cgiResponseCommonMultiCleanup(void **statepp);
 
 CgiStatus cgiJavascriptResponseCommon(HttpdConnData *connData, cJSON *jsroot, const char *jsObjName);
 
