cb069737c5b48292e3e909f769d19e26eda794f9
phatpaul <phatpaul@gmail.com>
2025-05-28 12:27:02 -0400

    cherry-pick memory leak fix

--- util/cgi_common.c
+++ util/cgi_common.c
@@ -234,13 +234,19 @@ CgiStatus cgiResponseCommonMulti(HttpdConnData *connData, void **statepp, const 
 		}
 	}
 
-	if (statep->len_to_send <= 0 || // finished sending
-		(statepp == NULL))			// or called without state pointer (single send)
+	// if called without state pointer (single send)
+	if (NULL == statepp)
 	{
-		ESP_LOGD(__func__, "freeing");
+		cgiResponseCommonMultiCleanup((void *)&statep);
+		return HTTPD_CGI_DONE;
+	}
+	// else if finished sending
+	if (statep->len_to_send <= 0)
+	{
 		cgiResponseCommonMultiCleanup(statepp);
 		return HTTPD_CGI_DONE;
 	}
+	// else still sending
 	return HTTPD_CGI_MORE;
 }
 
